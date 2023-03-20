04e8fe22f68b147f2f3d9d1287aca8f2468dacb4
Moeid Haider <moeidhaider9@gmail.com>
2023-03-20 14:33:52 +0000

    setup first 2 tests and passing one

---
+++ bankTest.test.js
@@ -0,0 +1,18 @@
+const Account = require('./bankTest.js');
+
+describe('Account', () => {
+  let account;
+
+  beforeEach(() => {
+    account = new Account();
+  });
+
+  it('should start with balance at 0', () =>{
+    expect(account.balance).toEqual(0);
+  });
+
+  it('should handle a single deposit', () => {
+    account.deposit(1000, "10/01/2023")
+    expect(account.balance).toEqual(1000)
+  });
+})
