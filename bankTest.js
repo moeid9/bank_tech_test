72444c611f48ee0d17f174460053a37931ddfcde
Moeid Haider <moeidhaider9@gmail.com>
2023-03-21 13:14:46 +0000

    Added bankStatement method with a running balance and amended tests to add acommodate the new running balance

--- bankTest.js
+++ bankTest.js
@@ -1,33 +1,52 @@
-require ('./transactions')
 class Account {
-    constructor() {
-        this.balance = 0
-        this.transactions = [];
-    }
+  constructor() {
+    this.balance = 0;
+    this.transactions = [];
+  }
 
-    deposit(amount, date) {
-        this.balance += amount 
-        const newTransaction = {
-            amount: amount,
-            date: date,
-            deposit: true,
-            withdraw: false,
-        }
-        this.transactions.push(newTransaction) 
-    }
+  deposit(date, amount) {
+    this.balance += amount;
+    const newTransaction = {
+      amount: amount,
+      date: date,
+      deposit: true,
+      withdraw: false,
+      balance: this.balance,
+    };
+    this.transactions.push(newTransaction);
+  }
 
-    withdraw(amount, date) {
-        this.balance -=  amount 
-        const newTransaction = {
-            amount: amount,
-            date: date,
-            deposit: false,
-            withdraw: true,
-        }
-        this.transactions.push(newTransaction) 
+  withdraw(date, amount) {
+    this.balance -= amount;
+    const newTransaction = {
+      amount: amount,
+      date: date,
+      deposit: false,
+      withdraw: true,
+      balance: this.balance,
+    };
+    this.transactions.push(newTransaction);
+  }
+
+  bankStatement() {
+    let statement = "date      || credit || debit || balance";
+    const transaction = this.transactions;
+    //for loop to create the statement output
+    for (let i = 0; i < transaction.length; i++) {
+      const deposit = transaction[i].deposit ? transaction[i].amount.toFixed(2) : '';
+      const withdraw = transaction[i].withdraw ? transaction[i].amount.toFixed(2) : '';
+      const balance = transaction[i].balance.toFixed(2);
+      statement += `\n${transaction[i].date}||${deposit ? `${deposit}` : '       '} ||${withdraw ? `${withdraw}` : '      '} || ${balance}`;
     }
+    return statement;
+  }
 }
-  
-  
-  
+
 module.exports = Account;
+
+// const account = new Account();
+// account.deposit("10/01/2023", 1000);
+// account.deposit("13/01/2023", 2000);
+// account.withdraw("14/01/2023", 500);
+
+// console.log(account.bankStatement());
--- bankTest.test.js
+++ bankTest.test.js
@@ -12,19 +12,29 @@ describe('Account', () => {
   });
 
   it('should handle a single deposit', () => {
-    account.deposit(1000, '10/10/2022')
+    account.deposit('10/10/2022', 1000)
     expect(account.balance).toEqual(1000)
   });
 
   it('should handle a single deposit and show date', () => {
-    account.deposit(1000, '10/10/2022')
+    account.deposit('10/10/2022', 1000)
     expect(account.balance).toEqual(1000)
-    expect(account.transactions).toEqual([{"amount": 1000, "date": "10/10/2022", "deposit": true, "withdraw": false}])
+    expect(account.transactions).toEqual([{"amount": 1000, "balance": 1000, "date": "10/10/2022", "deposit": true, "withdraw": false}])
   });
+
   it('should handle a single withdrawal and show date', () => {
-    account.deposit(1000, '10/10/2022')
-    account.withdraw(500, '12/10/2022')
+    account.deposit('10/10/2022', 1000)
+    account.withdraw('12/10/2022', 500)
     expect(account.balance).toEqual(500)
-    expect(account.transactions).toContainEqual({"amount": 500, "date": "12/10/2022", "deposit": false, "withdraw": true}) 
+    expect(account.transactions).toContainEqual({"amount": 500, "balance": 500, "date": "12/10/2022", "deposit": false, "withdraw": true}) 
   });
+
+  it('should return a display', () => {
+    account.deposit('10/10/2022', 1000)
+    account.withdraw('12/10/2022', 500)
+    expect(account.balance).toEqual(500)
+    expect(account.bankStatement()).toContain(`10/10/2022||1000.00 ||       || 1000.00`)
+    expect(account.bankStatement()).toContain(`12/10/2022||        ||500.00 || 500.00`) 
+  });
+
 })
